c8b3b77c03af9f67e76d3ad0abf9ca3cb0e64da8
grsjob <92042203+grsjob@users.noreply.github.com>
2022-06-20 14:17:22 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    extends: [
+        "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended"// Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        sourceType: "module",
+        ecmaVersion: 2020,
+        ecmaFeatures: {
+            jsx: true // Allows for the parsing of JSX
+        }
+    },
+    plugins: [
+        "@typescript-eslint",
+    ],
+    settings: {
+        react: {
+            version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+        }
+    },
+    // Fine tune rules
+    rules: {
+        "@typescript-eslint/no-var-requires": 0,
+    },
+};
---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../assets/scss/App.scss";
+
+const App = () => {
+  return (
+    <div className="app">
+    </div>
+  );
+};
+
+export default App;
